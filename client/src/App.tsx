fd5cf020dbbff4a879ba9a505f48f801e2f014f8
Meng <102444577+meng-sys@users.noreply.github.com>
2025-06-01 02:28:06 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,41 @@
 import { useEffect, useRef, useState } from 'react'
+import { Helmet } from 'react-helmet'
 import { getCookie } from 'typescript-cookie'
 import { DefaultParams, PathPattern, Route, Switch } from 'wouter'
 import Footer from './components/footer'
 import { Header } from './components/header'
 import { Padding } from './components/padding'
+import useTableOfContents from './hooks/useTableOfContents.tsx'
 import { client } from './main'
 import { CallbackPage } from './page/callback'
 import { FeedPage, TOCHeader } from './page/feed'
 import { FeedsPage } from './page/feeds'
 import { FriendsPage } from './page/friends'
+import { HashtagPage } from './page/hashtag.tsx'
+import { HashtagsPage } from './page/hashtags.tsx'
+import { Settings } from "./page/settings.tsx"
 import { TimelinePage } from './page/timeline'
 import { WritingPage } from './page/writing'
+import { ClientConfigContext, ConfigWrapper, defaultClientConfig } from './state/config.tsx'
 import { Profile, ProfileContext } from './state/profile'
 import { headersWithAuth } from './utils/auth'
 import { tryInt } from './utils/int'
-import { Settings } from "./page/settings.tsx";
-import { ClientConfigContext, ConfigWrapper } from './state/config.tsx'
- 
+import { SearchPage } from './page/search.tsx'
+import { Tips, TipsPage } from './components/tips.tsx'
+import { useTranslation } from 'react-i18next'
+
 function App() {
   const ref = useRef(false)
+  const { t } = useTranslation()
   const [profile, setProfile] = useState<Profile | undefined>()
-  const [config, setConfig] = useState<ConfigWrapper>(new ConfigWrapper({}))
-  const externalHTMLLoaded = useRef(false);
+  const [config, setConfig] = useState<ConfigWrapper>(new ConfigWrapper({}, new Map()))
   useEffect(() => {
     if (ref.current) return
     if (getCookie('token')?.length ?? 0 > 0) {
       client.user.profile.get({
         headers: headersWithAuth()
       }).then(({ data }) => {
-        if (data && typeof data != 'string') {
+        if (data && typeof data !== 'string') {
           setProfile({
             id: data.id,
             avatar: data.avatar || '',
@@ -41,61 +48,28 @@ function App() {
     const config = sessionStorage.getItem('config')
     if (config) {
       const configObj = JSON.parse(config)
-      const configWrapper = new ConfigWrapper(configObj)
+      const configWrapper = new ConfigWrapper(configObj, defaultClientConfig)
       setConfig(configWrapper)
     } else {
       client.config({ type: "client" }).get().then(({ data }) => {
-        if (data && typeof data != 'string') {
+        if (data && typeof data !== 'string') {
           sessionStorage.setItem('config', JSON.stringify(data))
-          const config = new ConfigWrapper(data)
+          const config = new ConfigWrapper(data, defaultClientConfig)
           setConfig(config)
         }
       })
     }
-    const ua = navigator.userAgent;
-    const hasFetchAction = /FetchAction/.test(ua);
-    if (!hasFetchAction && !externalHTMLLoaded.current) {
-      externalHTMLLoaded.current = true;
-      const musicScripts = [
-        { src: "https://npm.elemecdn.com/aplayer@1.10.1/dist/APlayer.min.js" },
-        { src: "https://npm.elemecdn.com/meting@2.0.1/dist/Meting.min.js" },
-      ];
-      const live2dScript = { src: "https://assets.xn--9iq088f7qityd.com/js/live2d.js" };
-    
-      Promise.all(musicScripts.map(script => new Promise<void>((resolve, reject) => {
-        const scriptElement = document.createElement('script');
-        scriptElement.src = script.src;
-        scriptElement.onload = () => resolve();
-        scriptElement.onerror = () => reject();
-        scriptElement.async = true;
-        document.body.appendChild(scriptElement);
-      }))).then(() => {
-        const metingScriptContent = `var meting_api='https://api.obdo.cc/meting/?server=:server&type=:type&id=:id';`;
-        const metingScript = document.createElement('script');
-        metingScript.textContent = metingScriptContent;
-        document.body.appendChild(metingScript);
-    
-        const externalContainer = document.createElement('div');
-        externalContainer.innerHTML = `
-          <div style="max-width: 450px; margin: auto;">
-            <meting-js autoplay="false" order="random" theme="#409EFF" list-folded="true" fixed="true" auto="https://music.163.com/#/playlist?id=8900628861"/>
-          </div>
-        `;
-        document.body.appendChild(externalContainer);
-      });
-    
-      const live2dScriptElement = document.createElement('script');
-      live2dScriptElement.src = live2dScript.src;
-      live2dScriptElement.async = true;
-      document.body.appendChild(live2dScriptElement);
-    
-    }
     ref.current = true
   }, [])
+  const favicon = `${process.env.API_URL}/favicon`;
   return (
     <>
       <ClientConfigContext.Provider value={config}>
         <ProfileContext.Provider value={profile}>
+          <Helmet>
+            {favicon &&
+              <link rel="icon" href={favicon} />}
+          </Helmet>
           <Switch>
             <RouteMe path="/">
               <FeedsPage />
@@ -108,6 +82,22 @@ function App() {
 
             <RouteMe path="/friends">
               <FriendsPage />
+            </RouteMe>
+
+            <RouteMe path="/hashtags">
+              <HashtagsPage />
+            </RouteMe>
+
+            <RouteMe path="/hashtag/:name">
+              {params => {
+                return (<HashtagPage name={params.name || ""} />)
+              }}
+            </RouteMe>
+
+            <RouteMe path="/search/:keyword">
+              {params => {
+                return (<SearchPage keyword={params.keyword || ""} />)
+              }}
             </RouteMe>
 
             <RouteMe path="/settings" paddingClassName='mx-4'>
@@ -132,18 +122,42 @@ function App() {
               <CallbackPage />
             </RouteMe>
 
-            <RouteMe path="/feed/:id" headerComponent={TOCHeader()} paddingClassName='mx-4'>
-              {params => {
-                return (<FeedPage id={params.id || ""} />)
+            <RouteWithIndex path="/feed/:id">
+              {(params, TOC, clean) => {
+                return (<FeedPage id={params.id || ""} TOC={TOC} clean={clean} />)
               }}
-            </RouteMe>
+            </RouteWithIndex>
 
-            <RouteMe path="/:alias" headerComponent={TOCHeader()} paddingClassName='mx-4'>
-              {params => {
+            <RouteWithIndex path="/:alias">
+              {(params, TOC, clean) => {
                 return (
-                  <FeedPage id={params.alias || ""} />
+                  <FeedPage id={params.alias || ""} TOC={TOC} clean={clean} />
                 )
               }}
+            </RouteWithIndex>
+
+            <RouteMe path="/user/github">
+              {_ => (
+                <TipsPage>
+                  <Tips value={t('error.api_url')} type='error' />
+                </TipsPage>
+              )}
+            </RouteMe>
+
+            <RouteMe path="/*/user/github">
+              {_ => (
+                <TipsPage>
+                  <Tips value={t('error.api_url_slash')} type='error' />
+                </TipsPage>
+              )}
+            </RouteMe>
+
+            <RouteMe path="/user/github/callback">
+              {_ => (
+                <TipsPage>
+                  <Tips value={t('error.github_callback')} type='error' />
+                </TipsPage>
+              )}
             </RouteMe>
 
             {/* Default route in a switch */}
@@ -167,11 +181,22 @@ function RouteMe({ path, children, headerComponent, paddingClassName }:
           <Padding className={paddingClassName}>
             {typeof children === 'function' ? children(params) : children}
           </Padding>
-          <Footer />  
+          <Footer />
         </>)
       }}
     </Route>
   )
+}
+
+
+function RouteWithIndex({ path, children }:
+  { path: PathPattern, children: (params: DefaultParams, TOC: () => JSX.Element, clean: (id: string) => void) => React.ReactNode }) {
+  const { TOC, cleanup } = useTableOfContents(".toc-content");
+  return (<RouteMe path={path} headerComponent={TOCHeader({ TOC: TOC })} paddingClassName='mx-4'>
+    {params => {
+      return children(params, TOC, cleanup)
+    }}
+  </RouteMe>)
 }
 
 export default App
